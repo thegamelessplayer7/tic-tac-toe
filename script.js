336213b2eec57c7f35f291d9aee0b5dc3f3df2ee
thegamelessplayer7 <thegamelessplayer7@gmail.com>
2021-12-30 12:57:46 -0600

    work on 3-in-a-row functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let winnerText = document.getElementById('announce-winner');
+
 var gameboard = (function() {
     let board = [];
     let player1Array = [];
@@ -20,61 +22,6 @@ var squares = (function() {
     let squareEight = document.getElementById('board-eight');
     let squareNine = document.getElementById('board-nine');
 
-
-   
-        
-        /*if (!event.target.textContent) {
-        event.target.id.textContent = 'X';
-        console.log(event.target.id);
-        gameboard.player1Array.push(event.target.textContent);
-        } else if (event.target.textContent === 'X') {
-            event.target.textContent = 'O';
-        } else if (event.target.textContent === 'O') {
-                event.target.textContent = 'X';
-            
-        }*/
-
-    
-
-/*
-    squareOne.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareOne.textContent = 'X';
-    }, {once: true});
-    squareTwo.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareTwo.textContent = 'X';
-    }, {once: true});
-    squareThree.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareThree.textContent = 'X';
-    }, {once: true});
-    squareFour.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareFour.textContent = 'X';
-    }, {once: true});
-    squareFive.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareFive.textContent = 'X';
-    }, {once: true});
-    squareSix.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareSix.textContent = 'X';
-    }, {once: true});
-    squareSeven.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareSeven.textContent = 'X';
-    }, {once: true});
-    squareEight.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareEight.textContent = 'X';
-    }, {once: true});
-    squareNine.addEventListener('click', (event) => {
-        gameboard.player1Array.push('x');
-        squareNine.textContent = 'X';
-    }, {once: true});
-*/
-
     return {
     
         squares,
@@ -92,35 +39,115 @@ var squares = (function() {
     
 })()
 
+
+let squareOne = document.getElementById('board-one');
+    let squareTwo = document.getElementById('board-two');
+    let squareThree = document.getElementById('board-three');
+    let squareFour = document.getElementById('board-four');
+    let squareFive = document.getElementById('board-five');
+    let squareSix = document.getElementById('board-six');
+    let squareSeven = document.getElementById('board-seven');
+    let squareEight = document.getElementById('board-eight');
+    let squareNine = document.getElementById('board-nine');
+
+
+   
+
 const gamepieces = {
     player1: 'X', 
-    player2: 'O'
-};
+    player2: 'O',
+}
 let player1Array = [];
-    let player2Array = [];
-let grid = document.getElementById('gameboard-container');
-let test;
-grid.addEventListener('click', (event) => {
-    test = document.querySelector(`div[id=${event.target.id}]`);
-    
-    if (!test.textContent && player1Array.length === 0 && player2Array.length === 0) {
-    player1Array.push(gamepieces.player1);
-    test.textContent = gamepieces.player1;
-    } else if (!test.textContent && player1Array.length > 0 && player2Array.length === 0) {
-        player2Array.push(gamepieces.player2)
-        test.textContent = gamepieces.player2;
-    } else if (!test.textContent && player1Array.length === player2Array.length) {
-        player1Array.push(gamepieces.player1);
-        test.textContent = gamepieces.player1;
-    } else if (!test.textContent && player1Array.length > player2Array.length) {
-        player2Array.push(gamepieces.player2);
-        test.textContent = gamepieces.player2;
-    }
-    console.log(`Player 1: ${player1Array}`, `Player 2: ${player2Array}`);
-})
+let player2Array = [];
 
-/*function gameplay {
+let playerOneBoard = [];
+let playerTwoBoard = [];
+
+const conditionsOne = ['board-one', 'board-five', 'board-nine'];
+const conditionsTwo = ['board-one', 'board-two', 'board-three'];
+const conditionsThree = ['board-one', 'board-four', 'board-seven'];
+const conditionsFour = ['board-three', 'board-five', 'board-seven'];
+const conditionsFive = ['board-three', 'board-six', 'board-nine'];
+const conditionsSix = ['board-seven', 'board-eight', 'board-nine'];
+const conditionsSeven = ['board-four', 'board-five', 'board-six'];
+const conditionsEight = ['board-two', 'board-five', 'board-eight'];
+
+
+let grid = document.getElementById('gameboard-container');
+let move;
+grid.addEventListener('click', (event) => {
+    move = document.querySelector(`div[id=${event.target.id}]`);
     
+    if (!move.textContent && player1Array.length === 0 && player2Array.length === 0) {
+    player1Array.push(gamepieces.player1);
+    move.textContent = gamepieces.player1;
+    playerOneBoard.push(move.id);
+    } else if (!move.textContent && player1Array.length > 0 && player2Array.length === 0) {
+        player2Array.push(gamepieces.player2)
+        move.textContent = gamepieces.player2;
+        playerTwoBoard.push(move.id);
+    } else if (!move.textContent && player1Array.length === player2Array.length) {
+        player1Array.push(gamepieces.player1);
+        move.textContent = gamepieces.player1;
+        playerOneBoard.push(move.id);
+    } else if (!move.textContent && player1Array.length > player2Array.length) {
+        player2Array.push(gamepieces.player2);
+        move.textContent = gamepieces.player2;
+        playerTwoBoard.push(move.id);
+    }
+  
+
+    if (playerOneBoard.length >= 3) {
+        if(playerOneBoard.includes('board-one' && 'board-two' && 'board-three')) {
+            winnerText.textContent = 'PlayerOne wins!';    
+    } else if(playerTwoBoard.includes('board-one' && 'board-two' && 'board-three')) {
+        winnerText.textContent = 'PlayerTwo wins!';
+    } else if (playerOneBoard.includes('board-one' && 'board-five' && 'board-nine')) {
+        winnerText.textContent = 'PlayerOne wins!';
+    } else if (playerTwoBoard.includes('board-one' && 'board-five' && 'board-nine')) {
+        winnerText.textContent = 'PlayerTwo wins!';
+    } else if (playerOneBoard.includes('board-one' && 'board-four' && 'board-seven')) {
+        winnerText.textContent = 'PlayerOne wins!';
+    } else if (playerTwoBoard.includes('board-one' && 'board-four' && 'board-seven')) {
+        winnerText.textContent = 'PlayerTwo wins!';
+    } else if(playerOneBoard.includes('board-three' && 'board-five' && 'board-seven')) {
+        winnerText.textContent = 'PlayerOne wins!';
+    } else if(playerTwoBoard.includes('board-three' && 'board-five' && 'board-seven')) {
+        winnerText.textContent = 'PlayerTwo wins!';
+    } else if(playerOneBoard.includes('board-three' && 'board-six' && 'board-nine')) {
+        winnerText.textContent = 'PlayerOne wins!';
+    } else if(playerTwoBoard.includes('board-three' && 'board-six' && 'board-nine')) {
+        winnerText.textContent = 'PlayerTwo wins!';
+    } else if(playerOneBoard.includes('board-seven' && 'board-eight' && 'board-nine')) {
+        winnerText.textContent = 'PlayerOne wins!';
+    } else if(playerTwoBoard.includes('board-seven' && 'board-eight' && 'board-nine')) {
+        winnerText.textContent = 'PlayerTwo wins!';
+    } else if(playerOneBoard.includes('board-four' && 'board-five' && 'board-six')) {
+        winnerText.textContent = 'PlayerOne wins!';
+    } else if(playerTwoBoard.includes('board-four' && 'board-five' && 'board-six')) {
+        winnerText.textContent = 'PlayerTwo wins!';
+    } else if(playerOneBoard.includes('board-two' && 'board-five' && 'board-eight')) {
+        winnerText.textContent = 'PlayerOne wins!';
+    } else if(playerTwoBoard.includes('board-two' && 'board-five' && 'board-eight')) {
+        winnerText.textContent = 'PlayerTwo wins!';
+    }
+}
+console.log(playerOneBoard, playerTwoBoard);
+});
+    
+    
+    
+
+    
+/*function gameplay {
+    1, 5, 9
+    1, 2, 3
+    1, 4, 7
+    3, 5, 7
+    3, 6, 9
+    7, 8, 9
+    4, 5, 6
+    2, 5, 8
 }*/
 
 /*
@@ -145,5 +172,11 @@ next click should be an O. If the last click saved the
 click to player1 array, then the next click should save
 to the player2 array.
 
+
+
+
+
+Create logic for 3 squares in a row. If there are 3 X's
+or 3 O's in a row, I want the game to end. 
 
 */
